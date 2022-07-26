2c26d8b050a00da882c24d3f304cdfdd861417af
REFAT SHAHRIAR <109792845+REFAT-156@users.noreply.github.com>
2022-07-27 02:39:16 +0600

    Update OLD.py

--- OLD.py
+++ OLD.py
@@ -1,24 +1,32 @@
 import os, platform
-
+ 
 try:
-
-    import requests
-
+ 
+        import requests
+ 
 except:
-
-    os.system('pip2 install requests')
-
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
 import requests
-
+ 
 bit = platform.architecture()[0]
-
-if bit == '64bit':
-
-    from OLD64 import menu
-
-    menu()
-
-elif bit == '32bit':
-
-    from OLD32 import menu
-  menu()
+ 
+if bit == "64bit":
+ 
+        from OLD64 import bye
+ 
+        bye()
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        from OLD32 import bye
+ 
+ 
+        bye()
+ 
+ 
